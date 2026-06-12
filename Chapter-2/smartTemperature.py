87fa94bbb039f4a40ba09e766dc975b8872e51a8
MD SHORIFUL ALAM ROBIN <typicalrobin@gmail.com>
2026-06-12 21:59:14 +0600

    Add smartTemperature and operators scripts

---
+++ Chapter-2/smartTemperature.py
@@ -0,0 +1,16 @@
+#take input celsius
+
+celsius= float(input("Enter The Value: "))
+
+
+#print fahrenhit equivalent of celsius
+
+fahren= (celsius*(9/5)) + 32
+
+print("Fahrenheit value is: ", fahren)
+
+
+
+
+
+
---
+++ operators.py
@@ -0,0 +1,51 @@
+# arithmatic operatoor
+
+x= 10
+y= 5
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+
+#comparison operators
+
+print(x==y) #False
+print(x>=y)
+print(x<=y)
+print(x>y) #true
+print(x<y) #false
+
+
+#logical operators
+
+print("And Operator Result", x>y and x<y)  #flase..2 tai right hole
+print("OR Operator Result",x>y or x<y) #true.. 1 ta right holey hobe.
+print("Not Operator Result", not (x>y)) #false
+
+
+#Assignment operator
+a= 2
+b= 3
+
+a= a+6
+a+= 6
+
+a= a-1
+a-= 1
+
+a= a/10
+a/= 10
+
+a=a*10
+a*=10
+
+
+
+
+
+
+
